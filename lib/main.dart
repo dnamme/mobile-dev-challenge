ed94b9bbcf0bd7e91be33802d6127ac4024fc991
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2020-10-17 01:13:12 +0800

    Initial tiles 3 and 4

--- lib/main.dart
+++ lib/main.dart
@@ -27,13 +27,12 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext buildContext) {
     return Scaffold(
-        // backgroundColor: Colors.white,
         body: new CustomPaint(
             painter: BackgroundPainter(),
             child: new SingleChildScrollView(
               padding: EdgeInsets.only(top: 50.0, bottom: 15.0),
-              // padding: EdgeInsets.fromLTRB(12.5, 50.0, 12.5, 10.0),
               child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   new Padding(
                       padding: EdgeInsets.only(left: 12.5, right: 12.5),
@@ -48,13 +47,12 @@ class _HomeState extends State<Home> {
                               ]),
                           child: new Column(children: [
                             new Stack(children: [
-                              new Image.asset(
-                                  'assets/img/Dog_and_Hooman.png'), // TODO: resize to fit
+                              new Image.asset('assets/img/Dog_and_Hooman.png'),
                               Positioned(
                                   left: 25.0,
                                   child: new Image.asset(
                                       'assets/icons/bookmark.png',
-                                      width: 25.0)), // TODO: find icon
+                                      width: 25.0)),
                             ]),
                             new Padding(
                                 padding:
@@ -80,7 +78,7 @@ class _HomeState extends State<Home> {
                                 ))
                           ]))),
                   new Padding(
-                      padding: EdgeInsets.fromLTRB(12.5, 40.0, 12.5, 25.0),
+                      padding: EdgeInsets.fromLTRB(12.5, 40.0, 12.5, 20.0),
                       child: new Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
@@ -139,7 +137,7 @@ class _HomeState extends State<Home> {
                                     new Container(
                                         child: new Padding(
                                             padding: EdgeInsets.fromLTRB(
-                                                5.0, 7.0, 5.0, 12.5),
+                                                0.0, 7.0, 5.0, 12.5),
                                             child: new Column(
                                                 crossAxisAlignment:
                                                     CrossAxisAlignment.start,
@@ -177,7 +175,9 @@ class _HomeState extends State<Home> {
                                     new Stack(children: [
                                       new Image.asset(
                                           'assets/img/Rat_Drops.png',
-                                          height: 90.0),
+                                          width: 90.0 / 253 * 516,
+                                          height: 90.0,
+                                          fit: BoxFit.fill),
                                       Positioned(
                                           top: 3.0,
                                           right: 7.5,
@@ -188,7 +188,7 @@ class _HomeState extends State<Home> {
                                     new Container(
                                         child: new Padding(
                                             padding: EdgeInsets.fromLTRB(
-                                                5.0, 7.0, 5.0, 12.5),
+                                                0.0, 7.0, 5.0, 12.5),
                                             child: new Column(
                                                 crossAxisAlignment:
                                                     CrossAxisAlignment.start,
@@ -211,7 +211,119 @@ class _HomeState extends State<Home> {
                                                 ])))
                                   ],
                                 )),
-                          ])))
+                          ]))),
+                  new Padding(
+                      padding: EdgeInsets.fromLTRB(12.5, 20.0, 12.5, 20.0),
+                      child: new Text('Curious',
+                          style: GoogleFonts.roboto(
+                              color: Color(0xff2d384c),
+                              fontSize: 17.5,
+                              fontWeight: FontWeight.w500))),
+                  new Padding(
+                      padding: EdgeInsets.only(left: 12.5),
+                      child: new SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: new Row(children: [
+                            new Container(
+                                decoration: BoxDecoration(
+                                    color: Color(0xff2d384c),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(15.0)),
+                                    boxShadow: [
+                                      new BoxShadow(
+                                          color: Colors.grey, blurRadius: 5.0)
+                                    ]),
+                                margin: EdgeInsets.only(right: 12.5),
+                                child: new Column(
+                                  children: [
+                                    new Stack(children: [
+                                      new Image.asset(
+                                          'assets/img/Koi_Desuu.png',
+                                          height: 90.0),
+                                      Positioned(
+                                          top: 3.0,
+                                          right: 7.5,
+                                          child: new Icon(Icons.star_rounded,
+                                              color: Color(0xfffdbd4e),
+                                              size: 35.0))
+                                    ]),
+                                    new Container(
+                                        child: new Padding(
+                                            padding: EdgeInsets.fromLTRB(
+                                                0.0, 7.0, 5.0, 12.5),
+                                            child: new Column(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: [
+                                                  new Text(
+                                                      'Do you know fish likes to...',
+                                                      style: GoogleFonts.roboto(
+                                                          color:
+                                                              Color(0xffffffff),
+                                                          fontSize: 12.5,
+                                                          fontWeight:
+                                                              FontWeight.w500)),
+                                                  new Text('8 minutes',
+                                                      style: GoogleFonts.roboto(
+                                                          color:
+                                                              Color(0xaaffffff),
+                                                          fontSize: 12.5,
+                                                          fontWeight:
+                                                              FontWeight.w500))
+                                                ])))
+                                  ],
+                                )),
+                            new Container(
+                                decoration: BoxDecoration(
+                                    color: Color(0xff2d384c),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(15.0)),
+                                    boxShadow: [
+                                      new BoxShadow(
+                                          color: Colors.grey, blurRadius: 5.0)
+                                    ]),
+                                margin: EdgeInsets.only(right: 12.5),
+                                child: new Column(
+                                  children: [
+                                    new Stack(children: [
+                                      new Image.asset(
+                                          'assets/img/Judger_Mantis.png',
+                                          width: 90.0 / 253 * 516,
+                                          height: 90.0,
+                                          fit: BoxFit.fill),
+                                      Positioned(
+                                          top: 3.0,
+                                          right: 7.5,
+                                          child: new Icon(Icons.star_rounded,
+                                              color: Color(0xfffdbd4e),
+                                              size: 35.0))
+                                    ]),
+                                    new Container(
+                                        child: new Padding(
+                                            padding: EdgeInsets.fromLTRB(
+                                                0.0, 7.0, 5.0, 12.5),
+                                            child: new Column(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: [
+                                                  new Text('...',
+                                                      style: GoogleFonts.roboto(
+                                                          color:
+                                                              Color(0xffffffff),
+                                                          fontSize: 12.5,
+                                                          fontWeight:
+                                                              FontWeight.w500)),
+                                                  new Text('9 minutes',
+                                                      style: GoogleFonts.roboto(
+                                                          color:
+                                                              Color(0xaaffffff),
+                                                          fontSize: 12.5,
+                                                          fontWeight:
+                                                              FontWeight.w500))
+                                                ])))
+                                  ],
+                                )),
+                          ]))),
                 ],
               ),
             )
@@ -271,39 +383,7 @@ class _HomeState extends State<Home> {
                   ],
                 )
               ],
-            ))
-        /*bottomNavigationBar: new BottomNavigationBar(
-            // TODO : work on sizing, convert to row
-            iconSize: 30.0,
-            showUnselectedLabels: true,
-            unselectedItemColor: Color(0xffc2c2c2),
-            unselectedLabelStyle: GoogleFonts.roboto(
-              fontSize: 12.5,
-              fontWeight: FontWeight.w500,
-            ),
-            selectedItemColor: Color(0xff2d384c),
-            selectedLabelStyle: GoogleFonts.roboto(
-              fontSize: 12.5,
-              fontWeight: FontWeight.w500,
-            ),
-            currentIndex: 0,
-            items: [
-              new BottomNavigationBarItem(
-                  icon:
-                      new Image.asset('assets/icons/discover.png', width: 30.0),
-                  label: 'Discover'),
-              new BottomNavigationBarItem(
-                  icon: new Image.asset('assets/icons/maps.png', width: 21.5),
-                  label: 'Maps'),
-              new BottomNavigationBarItem(
-                  icon: new Image.asset('assets/icons/learn.png', width: 42.5),
-                  label: 'Learn'),
-              new BottomNavigationBarItem(
-                  icon:
-                      new Image.asset('assets/icons/profile.png', width: 30.0),
-                  label: 'Profile'),
-            ])*/
-        );
+            )));
   }
 }
 
