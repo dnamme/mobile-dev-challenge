6063d1e4b2327d1a362dbc7fc4be35da9569bcc9
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2020-10-16 16:03:46 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/dnamme/mobile_development_challenge/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dnamme.mobile_development_challenge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+//import 'package:google_fonts/google_fonts.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext buildContext) {
+    return MaterialApp(
+      title: 'Mobile Development Challenge',
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext buildContext) {
+    return Scaffold(
+      //appBar: AppBar(),
+      body:
+          new Container(child: new Column(children: [new Text('Hello World')])),
+      /*bottomNavigationBar: new BottomNavigationBar(
+            iconSize: 30.00,
+            showUnselectedLabels: true,
+            unselectedItemColor: Color(0xffc2c2c2),
+            unselectedLabelStyle: //TextStyle(
+                GoogleFonts.roboto(
+                    fontSize: 12.50, fontWeight: FontWeight.w500),
+            selectedItemColor: Color(0xff2d384c),
+            selectedLabelStyle: //TextStyle(
+                GoogleFonts.roboto(
+                    fontSize: 12.50, fontWeight: FontWeight.w500),
+            currentIndex: 0,
+            items: [
+          new BottomNavigationBarItem(
+              icon: new Icon(Icons.ac_unit), label: 'Discover'),
+          new BottomNavigationBarItem(
+              icon: new Icon(Icons.place_outlined), label: 'Maps'),
+          new BottomNavigationBarItem(
+              icon: new Icon(Icons.ac_unit), label: 'Learn'),
+          new BottomNavigationBarItem(
+              icon: new Image.asset('assets/icons/profile_64px.png',
+                  width: 30.00, height: 30.00),
+              label: 'Profile')
+        ])*/
+    );
+  }
+}
