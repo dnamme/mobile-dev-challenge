f46ca429b141ce57ff1762f0bfe548877e6a7e6f
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2020-10-16 19:27:56 +0800

    Initial background and Tile 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:ffi';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(App());
@@ -27,10 +28,44 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext buildContext) {
     return Scaffold(
         appBar: AppBar(),
-        body: new Container(
-            child: new Column(children: [
-          new Text('Hello World', style: GoogleFonts.roboto())
-        ])),
+        body: new CustomPaint(
+            painter: BackgroundPainter(),
+            child: new SingleChildScrollView(
+              child: new Column(
+                children: [
+                  new Stack(
+                    children: [
+                      new Image.asset('assets/img/Dog_and_Hooman.png'),
+                      new Icon(Icons.bookmark,
+                          color: Color(0xff2d384c), size: 30.0)
+                    ],
+                  ),
+                  new Container(
+                      decoration: BoxDecoration(
+                          color: Color(0xfffdbd4e),
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(5.0),
+                              bottomRight: Radius.circular(5.0))),
+                      child: new Column(
+                        children: [
+                          new Text('10 Tips for Healthy, Happy Dogs',
+                              style: GoogleFonts.roboto(
+                                  fontSize: 25.0, fontWeight: FontWeight.w500)),
+                          new Text('12 minutes',
+                              style: GoogleFonts.roboto(
+                                  color: Color(0x432d384c),
+                                  fontSize: 12.50,
+                                  fontWeight: FontWeight.w500))
+                        ],
+                      ))
+                ],
+              ),
+            )
+            // #2d384c
+            // #2d384c 67%
+            // #Pet medical that you sho...
+            // #Do you know fish likes to...
+            ),
         bottomNavigationBar: new BottomNavigationBar(
             iconSize: 30.00,
             showUnselectedLabels: true,
@@ -59,5 +94,21 @@ class _HomeState extends State<Home> {
                   ),
                   label: 'Profile')
             ]));
+  }
+}
+
+class BackgroundPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = new Paint();
+
+    paint.color = Color(0xfffdbd4e);
+
+    canvas.drawRect(new Rect.fromLTRB(0, 0, size.width, 200), paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
   }
 }
