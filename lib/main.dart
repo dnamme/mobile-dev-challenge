5bf82a686c3b4394a9d130993d74a1aa6abb4067
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2020-10-16 16:28:02 +0800

    Add bottomnavbar without styles

--- lib/main.dart
+++ lib/main.dart
@@ -24,35 +24,34 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext buildContext) {
     return Scaffold(
-      appBar: AppBar(),
-      body: new Container(
-          child: new Column(children: [
-        new Text('Hello World', style: GoogleFonts.roboto())
-      ])),
-      /*bottomNavigationBar: new BottomNavigationBar(
-            iconSize: 30.00,
-            showUnselectedLabels: true,
-            unselectedItemColor: Color(0xffc2c2c2),
-            unselectedLabelStyle: //TextStyle(
-                GoogleFonts.roboto(
-                    fontSize: 12.50, fontWeight: FontWeight.w500),
-            selectedItemColor: Color(0xff2d384c),
-            selectedLabelStyle: //TextStyle(
-                GoogleFonts.roboto(
-                    fontSize: 12.50, fontWeight: FontWeight.w500),
+        appBar: AppBar(),
+        body: new Container(
+            child: new Column(children: [
+          new Text('Hello World', style: GoogleFonts.roboto())
+        ])),
+        bottomNavigationBar: new BottomNavigationBar(
+            // iconSize: 30.00,
+            // showUnselectedLabels: true,
+            // unselectedItemColor: Color(0xffc2c2c2),
+            // unselectedLabelStyle: //TextStyle(
+            //     GoogleFonts.roboto(
+            //         fontSize: 12.50, fontWeight: FontWeight.w500),
+            // selectedItemColor: Color(0xff2d384c),
+            // selectedLabelStyle: //TextStyle(
+            //     GoogleFonts.roboto(
+            //         fontSize: 12.50, fontWeight: FontWeight.w500),
             currentIndex: 0,
             items: [
-          new BottomNavigationBarItem(
-              icon: new Icon(Icons.ac_unit), label: 'Discover'),
-          new BottomNavigationBarItem(
-              icon: new Icon(Icons.place_outlined), label: 'Maps'),
-          new BottomNavigationBarItem(
-              icon: new Icon(Icons.ac_unit), label: 'Learn'),
-          new BottomNavigationBarItem(
-              icon: new Image.asset('assets/icons/profile_64px.png',
-                  width: 30.00, height: 30.00),
-              label: 'Profile')
-        ])*/
-    );
+              new BottomNavigationBarItem(
+                  icon: new Icon(Icons.ac_unit), label: 'Discover'),
+              new BottomNavigationBarItem(
+                  icon: new Icon(Icons.place_outlined), label: 'Maps'),
+              new BottomNavigationBarItem(
+                  icon: new Icon(Icons.ac_unit), label: 'Learn'),
+              new BottomNavigationBarItem(
+                  icon: new Image.asset('assets/icons/profile_64px.png',
+                      width: 30.00, height: 30.00),
+                  label: 'Profile')
+            ]));
   }
 }
